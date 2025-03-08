6e982cfb181165ebfd7798f717ed159080118796
Misbahhassan451 <misbahhassan520@gmail.com>
2025-03-09 00:29:59 +0500

    Add files via upload

---
+++ codealpha task1.py
@@ -0,0 +1,8 @@
+# Fibonacci generator using recursion
+def fibonacci_generator(a=0, b=1):
+    while True:
+        yield a
+        a, b = b, a + b
+fib_gen = fibonacci_generator()
+for _ in range(10):
+    print(next(fib_gen))
